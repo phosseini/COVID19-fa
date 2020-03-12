22a3bc8356f9a9eb41efa26ec59f56040190bc73
phosseini <pdr.hosseini@gmail.com>
2020-03-12 19:43:37 -0400

    adding data loader

---
+++ data_reader.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import data_utils as du
+
+
+class DataLoader:
+    def __init__(self):
+        self.data_path = "data/"
+
+    def load_data(self, count=500):
+        """
+        reading tweets' excel data
+        :return:
+        """
+
+        df_filtered = pd.DataFrame(
+            columns=["id", "tweet_url", "text", "user_description", "user_followers_count", "user_friends_count",
+                     "user_location", "user_verified"])
+
+        files = ["part1.xlsx"]
+
+        filters = {"tweet_type": ["original"], "lang": ["fa"]}
+
+        tags = du.get_hashtags()
+        tags = [k for k, v in tags.items() if "fa" in v]
+
+        for file in files:
+            df = pd.read_excel(self.data_path + file, nrows=count)
+
+            for index, row in df.iterrows():
+                if row["tweet_type"] in filters["tweet_type"] and row["lang"] in filters["lang"] and any(
+                        tag in row["text"] for tag in tags):
+                    df_filtered = df_filtered.append({"id": row["id"], "tweet_url": row["tweet_url"],
+                                                      "text": row["text"], "user_description": row["user_description"],
+                                                      "user_followers_count": row["user_followers_count"],
+                                                      "user_friends_count": row["user_friends_count"],
+                                                      "user_location": row["user_location"],
+                                                      "user_verified": row["user_verified"]},
+                                                     ignore_index=True)
+        return df
