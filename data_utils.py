d3be883def152209e18c9550eca0282e05c88cbe
phosseini <pdr.hosseini@gmail.com>
2020-04-09 04:36:34 -0400

    updating data utils

--- data_utils.py
+++ data_utils.py
@@ -2,6 +2,8 @@ import copy
 import pandas as pd
 
 from datetime import timedelta
+from data_reader import DataLoader
+from pre_processing import standardize_tweet_time
 
 
 # def get_hashtags():
@@ -34,3 +36,20 @@ def get_time_bins(start_date, bin_size, bin_length_day):
         bins.append([pd.to_datetime(a), pd.to_datetime(b)])
         a = copy.deepcopy(b)
     return bins
+
+
+def tweets_count_by_day():
+    """
+    getting count of tweets per day
+    :return:
+    """
+
+    df = DataLoader().load_tweets(n_count=1000000)
+
+    df['created_at'] = df['created_at'].apply(standardize_tweet_time)
+    df['created_at'] = pd.to_datetime(df['created_at'], format='%Y-%m-%d %H:%M:%S')
+
+    tweets_created_at = pd.to_datetime(df['created_at'])
+    tweets_counts_df = tweets_created_at.groupby(tweets_created_at.dt.floor('d')).size().reset_index(name='count')
+
+    return tweets_counts_df
